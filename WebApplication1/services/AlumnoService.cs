843f84dd220874d9bea1c426b478fe795f8da1c2
rubenprzz <rubenfernandezp1703@gmail.com>
2024-12-07 06:12:09 +0100

    SERVICE

--- WebApplication1/services/AlumnoService.cs
+++ WebApplication1/services/AlumnoService.cs
@@ -24,6 +24,7 @@ namespace WebApplication1.Services
        
         
 
+        
         public async Task<List<AlumnoViewModel>> ObtenerTodosLosAlumnos()
         {
             return await _context.Alumnos
--- WebApplication1/viewModels/AlumnoViewModel.cs
+++ WebApplication1/viewModels/AlumnoViewModel.cs
@@ -4,27 +4,27 @@ namespace WebApplication1.ViewModels
 {
     public class AlumnoViewModel
     {
-        public int Id { get; set; }
-        public string Nombre { get; set; }
-        public string Apellidos { get; set; }
-        public string Dni { get; set; }
-        public DateOnly FechaNacimiento { get; set; }
-        public string Email { get; set; }
-        public string Telefono { get; set; }
-        public List<string> Padres { get; set; } = new List<string>();  
-        public string Curso { get; set; }  
-        public List<string> Justificantes { get; set; }
+        public int? Id { get; set; }
+        public string? Nombre { get; set; }
+        public string? Apellidos { get; set; }
+        public string? Dni { get; set; }
+        public DateOnly? FechaNacimiento { get; set; }
+        public string? Email { get; set; }
+        public string? Telefono { get; set; }
+        public List<string>? Padres { get; set; } = new List<string>();  
+        public string? Curso { get; set; }  
+        public List<string>? Justificantes { get; set; }
         public List<int>? Notas { get; set; }    // Notas del alumno
-        public int CantidadRetrasos { get; set; } // Número de retrasos
-        private string _urlFotoL; // Campo privado para almacenar la URL relativa
+        public int? CantidadRetrasos { get; set; } // Número de retrasos
+        private string? _urlFotoL; // Campo privado para almacenar la URL relativa
 
-        public string UrlFotoL
+        public string? UrlFotoL
         {
             get => _urlFotoL;
             set => _urlFotoL = value;
         }
 
-        public string urlFoto
+        public string? urlFoto
         {
             get => $"http://localhost:5202{_urlFotoL}";
             set
